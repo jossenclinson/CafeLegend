c0371d1452dae51528d28dddf6493cd04fbf0355
jossenclinson <jossenclinson@yahoo.co.id>
2022-08-01 10:09:03 +0700

    Initial Commit

--- app/src/main/java/com/example/cafelegend/Item.java
+++ app/src/main/java/com/example/cafelegend/Item.java
@@ -5,17 +5,22 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class Item extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     DrawerLayout drawerLayout;
     NavigationView navView;
     Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter fragmentAdapter;
 
     void setDrawer(){
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
@@ -28,18 +33,45 @@ public class Item extends AppCompatActivity implements NavigationView.OnNavigati
     }
 
     void init(){
+        //Side Navbar
         drawerLayout = findViewById(R.id.drawerLayoutItem);
         navView = findViewById(R.id.navView);
         toolbar = findViewById(R.id.toolbar);
-    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item);
+        //Tab Layout
+        tabLayout = findViewById(R.id.tabLayoutItem);
+        viewPager2 = findViewById(R.id.viewPager2Item);
+        fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), getLifecycle());
+        viewPager2.setAdapter(fragmentAdapter);
 
-        init();
-        setDrawer();
+        tabLayout.addTab(tabLayout.newTab().setText("Appetizer"));
+        tabLayout.addTab(tabLayout.newTab().setText("Main Course"));
+        tabLayout.addTab(tabLayout.newTab().setText("Beverage"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 
     @Override
@@ -63,5 +95,14 @@ public class Item extends AppCompatActivity implements NavigationView.OnNavigati
                 return true;
         }
         return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+        init();
+        setDrawer();
     }
 }
