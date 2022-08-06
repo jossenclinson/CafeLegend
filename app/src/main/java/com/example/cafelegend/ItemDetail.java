30b7fde771d3746e52dddc813d77241fd449f558
Fernando Clemente <fernandoclemente73@gmail.com>
2022-08-06 09:36:44 +0700

    Make Item Detail

--- app/src/main/java/com/example/cafelegend/ItemDetail.java
+++ app/src/main/java/com/example/cafelegend/ItemDetail.java
@@ -1,8 +1,12 @@
 package com.example.cafelegend;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,9 +16,12 @@ public class ItemDetail extends AppCompatActivity {
     ImageView backItemIV, detailImageIV;
     TextView detailNameTV, detailPriceTV, detailDescTV;
     Bundle extras;
+    Button buyBtn;
+    EditText quantityET ;
 
     void init(){
         backItemIV = findViewById(R.id.backItemIV);
+        buyBtn = findViewById(R.id.buyBtn);
 
         detailImageIV = findViewById(R.id.detailImageIV);
         detailNameTV = findViewById(R.id.detailNameTV);
@@ -26,11 +33,26 @@ public class ItemDetail extends AppCompatActivity {
         detailNameTV.setText(extras.getString("detailName"));
         detailPriceTV.setText("IDR " + extras.getInt("detailPrice"));
         detailDescTV.setText(extras.getString("detailDesc"));
+
+        quantityET = findViewById(R.id.quantityET);
     }
 
     void eventBtn(){
         backItemIV.setOnClickListener(x -> {
             finish();
+        });
+    }
+
+    void eventBuyBtn(){
+        buyBtn.setOnClickListener(x ->{
+            if (quantityET.getText().toString().isEmpty()){
+                alert("Quantity must be greater than 0");
+            }else {
+                Integer quantity = Integer.parseInt(quantityET.getText().toString());
+                if (quantity <= 0) {
+                    alert("Quantity must be greater than 0");
+                }
+            }
         });
     }
 
@@ -41,5 +63,20 @@ public class ItemDetail extends AppCompatActivity {
 
         init();
         eventBtn();
+        eventBuyBtn();
+    }
+
+    private void alert(String message){
+        AlertDialog alertDialog = new AlertDialog.Builder(ItemDetail.this)
+                .setTitle("ERROR")
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create();
+        alertDialog.show();
     }
 }
