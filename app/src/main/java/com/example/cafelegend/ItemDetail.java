dcfe4c2f8eb36f302c164f0da231df1b523d74fb
jossenclinson <jossenclinson@yahoo.co.id>
2022-08-15 10:30:10 +0700

    add dialog box success buy

--- app/src/main/java/com/example/cafelegend/ItemDetail.java
+++ app/src/main/java/com/example/cafelegend/ItemDetail.java
@@ -52,7 +52,7 @@ public class ItemDetail extends AppCompatActivity {
                 if (quantity <= 0) {
                     alert("Quantity must be greater than 0");
                 }else{
-                    success("Success Buy " + extras.getString("detailName"));
+                    success("Success Buy " + quantity + " " + extras.getString("detailName"));
                 }
             }
         });
